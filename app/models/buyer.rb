03157eb81ba3ad677c902ceeb115b3994c885958
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-21 20:33:04 +0800

    added associations and checker methods

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,8 +1,20 @@
 class Buyer < ApplicationRecord
   belongs_to :admin
+  has_many :buyer_stocks
+  has_many :stocks, through: :buyer_stocks
   validates :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def stock_already_bought?(stock_id, buyer_id)
+    stock = BuyerStock.check_db(stock_id, buyer_id)
+    return false unless stock
+    BuyerStock.where(stock_id: stock_id, buyer_id: buyer_id).exists?
+  end
+
+  def can_buy_stock?(stock_id, broker_id)
+    !stock_already_bought?(stock_id, broker_id)
+  end         
 end
