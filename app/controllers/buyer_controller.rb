49b6450984ce8154b1e967e6d54735e417e48588
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-21 20:32:12 +0800

    added portfolio controller

--- app/controllers/buyer_controller.rb
+++ app/controllers/buyer_controller.rb
@@ -2,5 +2,10 @@ class BuyerController < ApplicationController
   before_action :authenticate_buyer!
 
   def index
+    @all_stocks = Stock.all
+  end
+
+  def portfolio
+    @bought_stocks = current_buyer.stocks
   end
 end
