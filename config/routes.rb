5b3942ca08da5a346eb7b00f85648cbf8b33c702
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-21 20:39:51 +0800

    added buyer routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # resources :buyer_stocks, only: [:create]
+  resources :buyer_stocks
   resources :broker_stocks, only: [:create, :destroy]
   devise_for :admins, :controllers => { :sessions => :sessions }
   devise_for :brokers, :controllers => { :registrations => :registrations, :sessions => :sessions }
@@ -6,10 +8,13 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'search_stock' => 'stocks#stock_search'
   get '/admin/index' => 'admin#index'
-  get '/buyer/index' => 'buyer#index'
 
   # Broker
   get '/broker/index' => 'broker#index'
   get '/broker/portfolio' => 'broker#portfolio'
+
+  # Buyer
+  get '/buyer/index' => 'buyer#index'
+  get '/buyer/portfolio' => 'buyer#portfolio'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
