43caebebd49a52f76ed865fd25cd02043688ffd3
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-23 04:54:19 +0800

    added show for broker_stocks resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # resources :buyer_stocks, only: [:create]
   resources :buyer_stocks
-  resources :broker_stocks, only: [:create, :destroy]
+  resources :broker_stocks, only: [:create, :destroy, :show]
   devise_for :admins, :controllers => { :sessions => :sessions }
   devise_for :brokers, :controllers => { :registrations => :registrations, :sessions => :sessions }
   devise_for :buyers, :controllers => { :registrations => :registrations, :sessions => :sessions }
