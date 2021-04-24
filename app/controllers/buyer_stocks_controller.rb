558f57c764939a97c1b1b4e06de39bb29b41a25d
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-25 00:12:01 +0800

    added broker_name to Transactions

--- app/controllers/buyer_stocks_controller.rb
+++ app/controllers/buyer_stocks_controller.rb
@@ -11,13 +11,13 @@ class BuyerStocksController < ApplicationController
       else
         new_total = existing_stock.quantity + params[:stock][:quantity].to_i
         existing_stock.update(quantity: new_total)
-        @broker_transaction = Transaction.create(broker_id: stock.added_by, stock_id: stock.id, buyer_id: current_buyer.id, ticker: stock.ticker, company_name: stock.name, stock_price: stock.last_price, quantity: params[:stock][:quantity])
+        @broker_transaction = Transaction.create(broker_id: stock.added_by, broker_name: stock.brokers.first.broker_name, stock_id: stock.id, buyer_id: current_buyer.id, ticker: stock.ticker, company_name: stock.name, stock_price: stock.last_price, quantity: params[:stock][:quantity])
         flash[:notice] = 'Successfully bought stock.'
         redirect_to buyer_portfolio_path
       end
     else
       @buyer_stock = BuyerStock.create(buyer_id: current_buyer.id, stock_id: stock.id, quantity: params[:stock][:quantity], stock_price: stock.last_price, broker_id: stock.added_by)
-      @broker_transaction = Transaction.create(broker_id: stock.added_by, stock_id: stock.id, buyer_id: current_buyer.id, ticker: stock.ticker, company_name: stock.name, stock_price: stock.last_price, quantity: params[:stock][:quantity])
+      @broker_transaction = Transaction.create(broker_id: stock.added_by, broker_name: stock.brokers.first.broker_name, stock_id: stock.id, buyer_id: current_buyer.id, ticker: stock.ticker, company_name: stock.name, stock_price: stock.last_price, quantity: params[:stock][:quantity])
       flash[:notice] = "Stock #{stock.name} was successfully bought."
       redirect_to buyer_portfolio_path
     end
