7af8d716c68ea73ff82e6616d060f3bf3baa3658
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-25 00:12:27 +0800

    customized destroy method for devise to delete broker stocks when broker is deleted

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -28,6 +28,17 @@ class RegistrationsController < Devise::RegistrationsController
       end
     end
 
+    def destroy
+      if resource.account_type == "broker"
+        Stock.where(added_by: resource.id).destroy_all
+      end
+      resource.destroy
+      Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+      set_flash_message! :notice, :destroyed
+      yield resource if block_given?
+      respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
+    end
+
     protected
 
     def after_sign_in_path_for(resource)
