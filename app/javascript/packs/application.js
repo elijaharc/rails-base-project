7531de81b7f41e788c1f8df2bf7f2e71e2234cda
mcndev <mcnikkopinlacnetflix@gmail.com>
2021-04-28 03:18:29 +0800

    Fixed alert error color, added number to currency method on views, nav improvements

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-global.toastr = require("toastr")
+// global.toastr = require("toastr")
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "@fortawesome/fontawesome-free/css/all"
+
+// import './src/application.scss'
+import toastr from 'toastr'
+window.toastr = toastr
 
 Rails.start()
 Turbolinks.start()
