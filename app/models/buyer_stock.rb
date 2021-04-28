6ce0add1b04500302b2d02317bf15c60433c4092
mcndev <mcnikkopinlacnetflix@gmail.com>
2021-04-29 06:07:29 +0800

    Added company_info fetch for buyer view

--- app/models/buyer_stock.rb
+++ app/models/buyer_stock.rb
@@ -5,4 +5,14 @@ class BuyerStock < ApplicationRecord
   def self.check_db(stock_id, buyer_id)
     find_by(stock_id: stock_id, buyer_id: buyer_id)
   end
+
+  def self.check_info(ticker)
+    client = IEX::Api::Client.new(
+      publishable_token: ENV["CLOUD_PUBLISHABLE_KEY"],
+      secret_token: ENV["CLOUD_SECRET_KEY"],
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+      company = client.company(ticker)
+  end
+
 end
