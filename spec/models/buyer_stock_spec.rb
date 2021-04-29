cf9720ebfcf1f560cfae9c07f2c056dbbbde59ef
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-29 21:13:46 +0800

    added RSpec test for check_info method

--- spec/models/buyer_stock_spec.rb
+++ spec/models/buyer_stock_spec.rb
@@ -21,4 +21,18 @@ RSpec.describe BuyerStock, type: :model do
       end
     end
   end
+
+  describe 'check_info' do
+    context 'when requesting for valid company information from IEX Gem' do
+      it 'returns company information object' do
+        expect(described_class.check_info('GOOG')).to be_a_kind_of Object
+      end
+    end
+
+    context 'when requesting for invalid company information from IEX Gem' do
+      it 'returns nil' do
+        expect(described_class.check_info('asdfjkl')).to be_nil
+      end
+    end
+  end
 end
