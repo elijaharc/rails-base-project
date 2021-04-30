f0e34f8616aef6d3b7c9529cb6fb8467b7f130a0
Elijah Arcedera <eli.arcedera@gmail.com>
2021-05-01 01:12:33 +0800

    added factory for broker_two

--- spec/factories/broker_factory.rb
+++ spec/factories/broker_factory.rb
@@ -1,12 +1,21 @@
 FactoryBot.define do
   factory :broker do
-    id { 1 }
-    broker_name { 'Test Broker' }
-    email { 'testbroker@gmail.com' }
     password { 'password' }
     account_type { 'broker' }
     association :admin
     confirmed_at { 'Sun, 25 Apr 2021 15:48:03.330499000 CST +08:00' }
     confirmation_token { 'U3dfFZuim_uBNPhyAEzW' }
+
+    factory :broker_one do
+      broker_name { 'Test Broker One' }
+      id { 1 }
+      email { 'testbroker1@gmail.com' }
+    end
+
+    factory :broker_two do
+      broker_name { 'Test Broker Two' }
+      id { 2 }
+      email { 'testbroker2@gmail.com' }
+    end
   end
 end
